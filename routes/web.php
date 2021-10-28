2b5ef6d20f43e71024dfa77b81102ddb742805f7
Andrius Usvaltas <andrius.usvaltas@vdu.lt>
2021-10-28 14:19:15 +0300

    2. Added route wildcard constrains and cache.

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,14 @@ Route::get('/', function () {
 });
 
 Route::get('/posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    if (! file_exists($path)) {
+    if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         return redirect('/');
-//        abort(404);
     }
-    $post = file_get_contents($path);
 
-    return view('post', [
-        'post' => $post
-        ]
-    );
-});
+    $post = cache()->remember("post.{$slug}", 1200, function () use ($path) {
+        return file_get_contents($path);
+    });
+
+    return view('post', ['post' => $post]);
+
+})->where('post', '[A-z_\-]+');

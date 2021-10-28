50e49216067ed5abfce4d5acbe21e8ded6f1d6d4
Andrius Usvaltas <andrius.usvaltas@vdu.lt>
2021-10-28 13:50:23 +0300

    1. Made new Routes, Stored blog posts as html.

---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<article>
+    <p>
+        <?= $post; ?>
+    </p>
+    <a href="/">Go Back</a>
+</article>
+
+
+</body>

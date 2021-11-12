99f43c2c2f6617a86361213ff8585166c896e551
Andrius Usvaltas <andrius.usvaltas@vdu.lt>
2021-11-12 14:57:17 +0200

    4. Seeding database with Factories.

--- database/migrations/2021_11_11_093946_create_post_table.php
+++ database/migrations/2021_11_11_093946_create_post_table.php
@@ -16,6 +16,7 @@ class CreatePostTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id');
+            $table->foreignId('user_id');
             $table->string('slug')->unique();
             $table->timestamps();
             $table->string('title');
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,7 +5,7 @@
     <article>
         <h1>{!! $post->title !!}</h1>
         <div>
-            <p><a href="/categories/{{ $post->category->slug }}">{!! $post->category->name !!}</a></p>
+            <p>By <a href="">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{!! $post->category->name !!}</a></p>
         </div>
         <div>
             {!! $post->body !!}

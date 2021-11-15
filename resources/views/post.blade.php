0bd23b99506c215fa916398312e21bd04884f91b
Andrius Usvaltas <andrius.usvaltas@vdu.lt>
2021-11-15 10:22:56 +0200

    5. Added View All Posts by An Author.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,10 +5,12 @@
     <article>
         <h1>{!! $post->title !!}</h1>
         <div>
-            <p>By <a href="">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{!! $post->category->name !!}</a></p>
+            <p>
+                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{!! $post->category->name !!}</a>
+            </p>
         </div>
         <div>
-            {!! $post->body !!}
+            <p>{!! $post->body !!}</p>
         </div>
         <a href="/">Go Back</a>
     </article>

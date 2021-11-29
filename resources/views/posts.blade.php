704c1d9690c2f0b683f5b85a7e89a2c3ce1ae719
Andrius Usvaltas <andrius.usvaltas@hostinger.com>
2021-11-29 19:58:27 +0200

    1115 Integrate the Design (#4)
    * 1. Converting HTLM and CSS to Blade, Blade Componenets, Grid.
    
    * 2. Updated Post pages with the main design.
    
    * 3. Created dropdown button with Alpine.js.
    
    * 4. Extracting dropdown blade components.
    
    * 5. Mild changes and cleanup before merge.
    
    Co-authored-by: Andrius Usvaltas <andrius.usvaltas@vdu.lt>

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,13 @@
-@extends('components.layout')
+<x-layout>
 
-@section('content')
+    @include('_post-header')
 
-    @foreach($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {!! $post->title !!}
-                </a>
-            </h1>
-            <div>
-                <p>
-                    By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{!! $post->category->name !!}</a>
-                </p>
-            </div>
-            <div>
-                {{ $post->excerpt }}
-            </div>
-        </article>
-    @endforeach
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if($posts->count())
+            <x-posts-grid :posts="$posts" />
+        @else
+            <p class="text-center">No Posts yet :(</p>
+        @endif
+    </main>
 
-@endsection
+</x-layout>
